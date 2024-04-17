d34dfe6aa5a47600656930001ab4e07d83bc2382
Aayushi Garg <gargayushi2003@gmail.com>
2024-04-18 01:16:01 +0530

    totalAmountByCategory

--- server/controllers/transaction.js
+++ server/controllers/transaction.js
@@ -58,7 +58,7 @@ export const getTransactions = async (req, res) => {
       transactions,
     });
 
-   
+
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
@@ -68,7 +68,7 @@ export const getTransactions = async (req, res) => {
 
 export const addTransactions = async (req, res) => {
   try {
-    const { userId, cardId, transactionName, vendor, category,date,amount } = req.body;
+    const { userId, cardId, transactionName, vendor, category, date, amount } = req.body;
 
     // Create a new transaction instance
     const newTransaction = new Transaction({
@@ -93,11 +93,11 @@ export const addTransactions = async (req, res) => {
 export const updateTransactions = async (req, res) => {
   try {
     const { transactionId } = req.params; // Get the transaction ID from the request parameters
-    const _id=transactionId;
+    const _id = transactionId;
     const { userId, cardId, transactionName, vendor, category, date, amount } = req.body; // Get the updated transaction details from the request body
-    
+
     // Find the transaction by ID and update its fields
-    const updatedTransaction = await Transaction.findByIdAndUpdate({_id},{
+    const updatedTransaction = await Transaction.findByIdAndUpdate({ _id }, {
       userId: new mongoose.Types.ObjectId(userId),
       cardId: new mongoose.Types.ObjectId(cardId),
       transactionName,
@@ -122,12 +122,12 @@ export const updateCategoryTransactions = async (req, res) => {
     const { transactionId } = req.params;
     console.log(transactionId)
     const _id = transactionId;
-   
-     // Get the transaction ID from the request parameters
+
+    // Get the transaction ID from the request parameters
     const { category } = req.body; // Get the updated category from the request body
 
     // Find the transaction by ID and update its category
-    const updatedTransaction = await Transaction.findByIdAndUpdate({_id}, { category }, { new: true });
+    const updatedTransaction = await Transaction.findByIdAndUpdate({ _id }, { category }, { new: true });
 
     if (!updatedTransaction) {
       return res.status(404).json({ message: "Transaction not found" });
@@ -136,5 +136,27 @@ export const updateCategoryTransactions = async (req, res) => {
     res.status(200).json({ message: "Category updated successfully", transaction: updatedTransaction });
   } catch (error) {
     res.status(400).json({ message: error.message });
+  }
+};
+
+export const getTotalAmountByCategory = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const transactions = await Transaction.find({ userId });
+    const categoryTotals = {};
+
+    transactions.forEach((transaction) => {
+      const { category, amount } = transaction;
+      // If category is not already in categoryTotals, initialize it to 0
+      if (!categoryTotals[category]) {
+        categoryTotals[category] = 0;
+      }
+      // Add transaction amount to total for the category
+      categoryTotals[category] += amount;
+    });
+    res.status(200).json(categoryTotals);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
